15aabf901f9d16f22cada529991012be543fec07
estebantjx <104031838+estebantjx@users.noreply.github.com>
2023-04-27 09:33:02 -0500

    Add files via upload

--- LOGIN/login.php
+++ LOGIN/login.php
@@ -9,8 +9,8 @@
 		<label for="usuario">Usuario:</label>
 		<input type="text" id="usuario" name="usuario"><br>
 
-		<label for="contraseña">Password:</label>
-		<input type="contraseña" id="contraseña" name="contraseña"><br>
+		<label for="contraseña">Contraseña:</label>
+		<input type="password" id="contraseña" name="contraseña"><br>
 
 		<input type="submit" value="Login">
 	</form>
@@ -18,19 +18,16 @@
 session_start();
 
 if (isset($_POST['usuario']) && isset($_POST['contraseña'])) {
-	// Recuperar los datos de inicio de sesión del formulario
+
 	$username = $_POST['usuario'];
 	$password = $_POST['contraseña'];
 
-	// Validar los datos de inicio de sesión
 	if ($username === 'esteban' && $password === '123') {
-		// Iniciar sesión y redirigir al usuario a la página de destino
 		$_SESSION['usuario'] = $username;
 		header('Location: index.php');
 		exit();
 	} else {
-		// Mostrar un mensaje de error al usuario
-		$error_message = 'La contraseña o el correo son invalidos';
+		print 'Contraseña o usuario incorrectos';
 	}
 }
 
